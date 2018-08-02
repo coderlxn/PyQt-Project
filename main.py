dbc0313bb68b0d52f28e7acb8c1a6ffd60380bf0
Vector <anyetiangong@qq.com>
2018-08-02 19:32:58 +0800

    俄罗斯方块

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from PyQt5.QtGui import QIcon
 from login import LoginDialog
 import resources
 import config
+import Tetris
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyleSheet(config.global_qss)
     login_widget = LoginDialog()
     if login_widget.exec_() == QDialog.Accepted:
-        main_window = QMainWindow()
-        main_window.show()
-        icon = QIcon("/Resources/bg.jpg")
-        main_window.setWindowIcon(icon)
+        # main_window = QMainWindow()
+        # main_window.show()
+        # icon = QIcon("/Resources/bg.jpg")
+        # # main_window.setWindowIcon(icon)
+        tetris = Tetris.Tetris()
     else:
         sys.exit(0)
     sys.exit(app.exec_())
